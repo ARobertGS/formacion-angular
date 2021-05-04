7881e53eca85570423d8cf6442e0f22d5176a6cb
Robert2807 <a.robert.gs@gmail.com>
2021-05-03 20:52:33 -0500

    circle progress

--- angular-medium-session2/src/app/app.module.ts
+++ angular-medium-session2/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CircleProgressComponent } from './circle-progress/circle-progress.component';
 import { CircleProgressOptions, NgCircleProgressModule } from 'ng-circle-progress';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CircleProgressComponent,
+    CourseComponent,
   ],
   imports: [
     BrowserModule,
